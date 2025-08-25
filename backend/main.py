b7dd4314a40a9356505be07e10233d76952939d1
MKinanA <mkinanahsan@gmail.com>
2025-08-25 19:52:47 +0700

    Memperbaiki error path di FRONTEND_DIRECTORY

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from api import app as api
 from pathlib import Path
 
-FRONTENT_DIRECTORY = Path(__file__).parent / 'frontend' # Assuming that this file is located in `backend` and the static files for frontend are in `../frontend` relative to this file
+FRONTENT_DIRECTORY = Path(__file__).parent.parent / 'frontend' # Assuming that this file is located in `backend` and the static files for frontend are in `../frontend` relative to this file
 
 app = FastAPI()
 app.mount('/api', api)
