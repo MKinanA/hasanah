c9d430e63a9f172fe2351c479c7ec13871871bee
MKinanA <mkinanahsan@gmail.com>
2025-10-08 11:14:14 +0700

    Memisah logika pemeriksaan apakah file adalah teks ke fungsi terpisah

--- backend/helpers/file_is_text.py
+++ backend/helpers/file_is_text.py
@@ -3,14 +3,11 @@ from pathlib import Path
 from ..helpers.log import log
 
 def file_is_text(path: Path | str) -> bool:
-    path = Path(path)
-    is_text = True
+    if not (path := Path(path)).exists(): return False
     with open(path, 'rb') as file:
         for chunk in iter(lambda: file.read(64**2), b''):
             try: chunk.decode()
-            except UnicodeDecodeError:
-                is_text = False
-                break
-    return is_text
+            except UnicodeDecodeError: return False
+    return True
 
 print(log(__name__, 'loaded')) # File load log
