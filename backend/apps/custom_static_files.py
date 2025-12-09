9dd39c54efdaf694b107310fa98d8902771a7cb7
MKinanA <mkinanahsan@gmail.com>
2025-12-09 17:07:53 +0700

    Menggunakan `StaticFiles` mentah untuk `CustomStaticFiles` untuk sementara

--- backend/apps/custom_static_files.py
+++ backend/apps/custom_static_files.py
@@ -8,23 +8,25 @@ from random import choice as random
 from ..helpers.custom_html_templating import custom_html_templating, file_is_templatable_html
 from ..helpers.log import log
 
-CACHED_FILE_NAME_COMBINATION = 'abcdefghijklmnopqrstuvwxyz0123456789'
-CACHED_FILE_NAME_LENGTH = 16
+# CACHED_FILE_NAME_COMBINATION = 'abcdefghijklmnopqrstuvwxyz0123456789'
+# CACHED_FILE_NAME_LENGTH = 16
 
-class CustomStaticFiles(StaticFiles):
-    def get_path(self, scope) -> str:
-        path = super().get_path(scope)
-        return path
+# class CustomStaticFiles(StaticFiles):
+#     def get_path(self, scope) -> str:
+#         path = super().get_path(scope)
+#         return path
 
-    async def get_response(self, path, scope) -> Response:
-        path = Path(path).resolve()
-        cached = False
-        if file_is_templatable_html(path):
-            with open(cache_path := Path(str(self.directory))/f'cache/{str().join(random(CACHED_FILE_NAME_COMBINATION) for _ in range(CACHED_FILE_NAME_LENGTH))}{path.suffix}', 'wt') as cache_file: cache_file.write(custom_html_templating(path.read_text()))
-            path = cache_path
-            cached = True
-        response = await super().get_response(str(path), scope)
-        if cached: response.background = BackgroundTask(lambda: path.unlink(True))
-        return response
+#     async def get_response(self, path, scope) -> Response:
+#         path = Path(path).resolve()
+#         cached = False
+#         if file_is_templatable_html(path):
+#             with open(cache_path := Path(str(self.directory))/f'cache/{str().join(random(CACHED_FILE_NAME_COMBINATION) for _ in range(CACHED_FILE_NAME_LENGTH))}{path.suffix}', 'wt') as cache_file: cache_file.write(custom_html_templating(path.read_text()))
+#             path = cache_path
+#             cached = True
+#         response = await super().get_response(str(path), scope)
+#         if cached: response.background = BackgroundTask(lambda: path.unlink(True))
+#         return response
+
+CustomStaticFiles = StaticFiles
 
 print(log(__name__, 'loaded')) # File load log
