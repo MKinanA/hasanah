b88283cef53aa62e389721777d12668764962d06
MKinanA <mkinanahsan@gmail.com>
2025-12-09 14:44:03 +0700

    Menggabungkan backend/main.py ke backend/__init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,1 +1,17 @@
-from . import apps, models, seeders, helpers, run_schema_and_seed, main
+from . import apps, models, seeders, helpers, run_schema_and_seed
+
+from fastapi import FastAPI
+from pathlib import Path
+from .apps.api import api
+from .apps.custom_static_files import CustomStaticFiles
+from .helpers.log import log
+
+FRONTEND_DIRECTORY = Path(__file__).parent.parent / 'frontend' # `../frontend/`
+
+app = FastAPI()
+app.mount('/api', api)
+app.mount('/', CustomStaticFiles(directory=FRONTEND_DIRECTORY, html=True))
+
+print(log(__name__, 'loaded')) # File load log
+print(log(__name__, f'{FRONTEND_DIRECTORY = }'))
+print(log(__name__, f'{__package__ = }'))
