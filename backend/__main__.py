f9e249b9155c1a7513657c63370f4cd807340ad5
MKinanA <mkinanahsan@gmail.com>
2025-12-12 02:31:03 +0700

    Memindahkan DB connection test dari script model user ke __main__.py

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,8 +1,18 @@
-from uvicorn import run
+from asyncio import run as async_run
+from uvicorn import run as uvicorn_run
 from . import app
+from .helpers.db_connect import db_connect
+from .helpers.log import log
 
-if __name__ == "__main__": run(
-    app,
-    host='0.0.0.0',
-    port=8080,
-)
+if __name__ == "__main__":
+    print(log(__name__, 'Testing database connection...'))
+    async def init_connect():
+        async with db_connect(): pass
+    async_run(init_connect())
+
+    print(log(__name__, 'Starting app...'))
+    uvicorn_run(
+        app,
+        host='0.0.0.0',
+        port=8080,
+    )
