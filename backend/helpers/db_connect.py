f165c331ac41c530c64c409c5d1e66989c1f3b29
MKinanA <mkinanahsan@gmail.com>
2025-12-12 02:28:43 +0700

    Memperbaiki dan mengubah helper `db_connect` menjadi async context manager

--- backend/helpers/db_connect.py
+++ backend/helpers/db_connect.py
@@ -1,7 +1,10 @@
+from contextlib import asynccontextmanager
 import aiosqlite as sql
 from .get_package_path import get_package_path
 
-async def db_connect() -> sql.Connection:
+@asynccontextmanager
+async def db_connect():
     conn = await sql.connect(get_package_path(__name__, __file__)/'.db')
     conn.row_factory = sql.Row
-    return conn
+    try: yield conn
+    finally: await conn.close()
