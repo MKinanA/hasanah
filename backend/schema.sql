e7c121e15fcd0645421194595bc1343dd8790b49
MKinanA <mkinanahsan@gmail.com>
2025-12-13 23:06:21 +0700

    Menambahkan tabel `user_session` untuk data sesi auth user

--- backend/schema.sql
+++ backend/schema.sql
@@ -3,8 +3,8 @@ PRAGMA journal_mode=WAL;
 
 CREATE TABLE user (
     id INTEGER PRIMARY KEY NOT NULL,
-    username TEXT NOT NULL UNIQUE CHECK (username = lower(username)),
-    name TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE CHECK (username = lower(username) AND length(username) BETWEEN 1 AND 64),
+    name TEXT NOT NULL CHECK (length(name) BETWEEN 1 AND 64),
     password TEXT NOT NULL
 );
 
@@ -15,7 +15,13 @@ CREATE TABLE access (
 
 CREATE TABLE r_user_access (
     id INTEGER PRIMARY KEY NOT NULL,
+    user INTEGER NOT NULL UNIQUE REFERENCES user(id) ON DELETE CASCADE,
+    access INTEGER NOT NULL UNIQUE REFERENCES access(id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_session (
+    id INTEGER PRIMARY KEY NOT NULL,
     user INTEGER NOT NULL REFERENCES user(id) ON DELETE CASCADE,
-    access INTEGER NOT NULL REFERENCES access(id) ON DELETE CASCADE,
-    UNIQUE (user, access)
+    token TEXT NOT NULL UNIQUE,
+    last_active INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
 );
