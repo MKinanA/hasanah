6a2133c218046d461cb01738d7cd52e2ff77cdf6
MKinanA <mkinanahsan@gmail.com>
2025-12-23 10:30:12 +0700

    Menambahkan exception handler

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,14 +1,20 @@
 from . import apps, models, seeders, helpers, run_schema_and_seed
 
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from .apps.api import api
 from .apps.custom_static_files import CustomStaticFiles
 from .helpers.get_package_path import get_package_path
+from .helpers.api_response import api_response as mkresp
 from .helpers.log import log
 
 FRONTEND_DIRECTORY = get_package_path(__name__, __file__).parent / 'frontend' # `../frontend/`
 
 app = FastAPI()
+
+@app.exception_handler(Exception)
+async def exception_handler(request, exc: Exception) -> JSONResponse: return JSONResponse(status_code=getattr(exc, 'status_code', 500), content=mkresp('error', type(exc).__name__, str(exc)))
+
 app.include_router(api, prefix='/api')
 app.mount('/', CustomStaticFiles(directory=FRONTEND_DIRECTORY, html=True))
 
