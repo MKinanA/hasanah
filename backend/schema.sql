d8e7b7c7604a6772d3c18f6eda1d32da94490fa8
MKinanA <mkinanahsan@gmail.com>
2025-12-29 04:59:01 +0700

    Membuat semua table `STRICT`

--- backend/schema.sql
+++ backend/schema.sql
@@ -6,37 +6,37 @@ CREATE TABLE kv_store (
     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
     key TEXT UNIQUE NOT NULL,
     value TEXT NOT NULL
-);
+) STRICT;
 
 CREATE TABLE user (
     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
     username TEXT NOT NULL UNIQUE CHECK (username = lower(username) AND length(username) BETWEEN 1 AND 64),
     name TEXT NOT NULL CHECK (length(name) BETWEEN 1 AND 64),
     password TEXT NOT NULL
-);
+) STRICT;
 
 CREATE TABLE access (
     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL UNIQUE CHECK (name = lower(name))
-);
+) STRICT;
 
 CREATE TABLE r_user_access (
     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
     user INTEGER NOT NULL UNIQUE REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE,
     access INTEGER NOT NULL UNIQUE REFERENCES access(id) ON UPDATE CASCADE ON DELETE RESTRICT
-);
+) STRICT;
 
 CREATE TABLE user_session (
     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
     user INTEGER NOT NULL REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE,
     token TEXT NOT NULL UNIQUE,
     last_active INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))
-);
+) STRICT;
 
 CREATE TABLE zis_payment (
     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
     uuid TEXT UNIQUE NOT NULL
-);
+) STRICT;
 
 CREATE TABLE zis_payment_version (
     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
@@ -52,12 +52,12 @@ CREATE TABLE zis_payment_version (
     is_deleted INTEGER NOT NULL DEFAULT FALSE CHECK (is_deleted IN (TRUE, FALSE)),
     CONSTRAINT number_or_email_required CHECK (payer_number IS NOT NULL OR payer_email IS NOT NULL),
     UNIQUE (payment, version)
-);
+) STRICT;
 
 CREATE TABLE zis_payment_category (
     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL UNIQUE CHECK (name = lower(name))
-);
+) STRICT;
 
 CREATE TABLE zis_payment_line (
     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
@@ -66,4 +66,4 @@ CREATE TABLE zis_payment_line (
     category INTEGER NOT NULL REFERENCES zis_payment_category(id) ON UPDATE CASCADE ON DELETE RESTRICT,
     amount INTEGER NOT NULL CHECK (amount >= 0),
     note TEXT
-);
+) STRICT;
