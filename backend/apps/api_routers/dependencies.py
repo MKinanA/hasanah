69bbdeeaddc37a818eaf8bd39964f05a6fdd3b84
MKinanA <mkinanahsan@gmail.com>
2025-12-30 08:19:27 +0700

    Memperbarui dependencies

--- backend/apps/api_routers/dependencies.py
+++ backend/apps/api_routers/dependencies.py
@@ -1,5 +1,18 @@
 from fastapi import Request
+from json import JSONDecodeError
 from ...models.user import User
 
-async def auth(request: Request) -> 'User | None': return await User.get_by_session_auth(token) if type(token := (await request.json()).get('token')) == str else None
-async def json_body(request: Request) -> dict: return await request.json()
+class NoAuthToken(Exception): status_code = 401
+class UserSessionNotFound(Exception): status_code = 401
+class InvalidBodyFormat(Exception): status_code = 400
+
+async def auth(request: Request) -> User:
+    user = await User.get_by_session_auth(token) if type(token := (await request.json()).get('token')) == str else None
+    if user is None:
+        if token is None: raise NoAuthToken('Please provide a session token for this request.')
+        raise UserSessionNotFound('Can\'t find your session, try logging in again.')
+    return user
+
+async def json_body(request: Request) -> dict:
+    try: return await request.json()
+    except JSONDecodeError as e: raise InvalidBodyFormat(f'Failed to parse request body as JSON.\n{type(e).__name__}: {str(e)}') from e
