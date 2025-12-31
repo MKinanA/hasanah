1a328000ddb944a62e0abd076c202147c2a57bc2
MKinanA <mkinanahsan@gmail.com>
2025-12-31 17:49:38 +0700

    Minor update

--- backend/apps/api_routers/test.py
+++ backend/apps/api_routers/test.py
@@ -15,4 +15,4 @@ async def auth(request: Request, response: Response, user: User = Depends(auth))
     return mkresp('success', 'Authenticated', 'Token is valid.', username=user.username)
 
 @router.post('/request-body')
-async def request_body(request: Request, response: Response, body: dict = Depends(json_body)): return mkresp('success', 'Request Received', 'Request body has been parsed as JSON.', json=body, raw=(await request.body()).decode())
+async def request_body(request: Request, response: Response, body: dict = Depends(json_body)) -> dict: return mkresp('success', 'Request Received', 'Request body has been parsed as JSON.', json=body, raw=(await request.body()).decode())
