1f6eb307d03c2e074a3bd1ccee5da1265def8e47
MKinanA <mkinanahsan@gmail.com>
2026-01-05 18:12:30 +0700

    Memperbarui dependency agar bisa menerima auth token dari `authentication` header (prioritas)

--- backend/apps/dependencies.py
+++ backend/apps/dependencies.py
@@ -8,12 +8,14 @@ class UserSessionNotFound(Exception): status_code = 401
 class InvalidBodyFormat(Exception): status_code = 400
 
 async def auth(request: Request) -> User:
+    header_token = request.headers.get('authorization')
+    header_token = (split_header_token[1] if len(split_header_token) >= 2 else split_header_token[0] if len(split_header_token) >= 1 else None) if type(header_token) == str and (split_header_token[0].lower() == 'bearer' if len(split_header_token := header_token.split()) >= 2 else True) else None
     cookie_token = request.cookies.get('token')
     json_token = (await request.json()).get('token')
-    if type(cookie_token) == str and type(json_token) == str and cookie_token != json_token: raise DifferentAuthTokens('The provided tokens in cookies and json body are different.')
-    user = (await User.get_by_session_auth(cookie_token)) if type(cookie_token) == str else (await User.get_by_session_auth(json_token)) if type(json_token) == str else None
+    if len(set(token for token in (header_token, cookie_token, json_token) if type(token) == str)) > 1: raise DifferentAuthTokens('The provided tokens in cookies and json body are different.')
+    user = (await User.get_by_session_auth(header_token)) if type(header_token) == str else (await User.get_by_session_auth(cookie_token)) if type(cookie_token) == str else (await User.get_by_session_auth(json_token)) if type(json_token) == str else None
     if user is None:
-        if str not in (type(cookie_token), type(json_token)): raise NoAuthToken('Please provide a session token for this request.')
+        if str not in (type(header_token), type(cookie_token), type(json_token)): raise NoAuthToken('Please provide a valid session token for this request, it could be via an `authentication` header with type `Bearer`, a `token` cookie, or a `token` field in the JSON body.')
         raise UserSessionNotFound('Can\'t find your session, try logging in again.')
     return user
 
