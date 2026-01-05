52801aabad3f975cadd4622bbacffa8711a8ccc0
MKinanA <mkinanahsan@gmail.com>
2026-01-05 16:09:52 +0700

    Memperbarui dependency `auth` agar bisa menerima token dari cookies (yang menjadi prioritas) selain dari json body

--- backend/apps/dependencies.py
+++ backend/apps/dependencies.py
@@ -3,13 +3,17 @@ from json import JSONDecodeError
 from ..models.user import User
 
 class NoAuthToken(Exception): status_code = 401
+class DifferentAuthTokens(Exception): status_code = 400
 class UserSessionNotFound(Exception): status_code = 401
 class InvalidBodyFormat(Exception): status_code = 400
 
 async def auth(request: Request) -> User:
-    user = await User.get_by_session_auth(token) if type(token := (await request.json()).get('token')) == str else None
+    cookie_token = request.cookies.get('token')
+    json_token = (await request.json()).get('token')
+    if type(cookie_token) == str and type(json_token) == str and cookie_token != json_token: raise DifferentAuthTokens('The provided tokens in cookies and json body are different.')
+    user = (await User.get_by_session_auth(cookie_token)) if type(cookie_token) == str else (await User.get_by_session_auth(json_token)) if type(json_token) == str else None
     if user is None:
-        if token is None: raise NoAuthToken('Please provide a session token for this request.')
+        if str not in (type(cookie_token), type(json_token)): raise NoAuthToken('Please provide a session token for this request.')
         raise UserSessionNotFound('Can\'t find your session, try logging in again.')
     return user
 
