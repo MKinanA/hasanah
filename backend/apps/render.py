10972b0f3f1224be74d4f8f1f89d774e0d5b7bfe
MKinanA <mkinanahsan@gmail.com>
2026-01-11 12:40:29 +0700

    Menu nav di header sekarang dibuat secara dinamis di sisi server berdasarkan access user

--- backend/apps/render.py
+++ backend/apps/render.py
@@ -1,6 +1,8 @@
 from fastapi.responses import Response, HTMLResponse
 from jinja2 import Environment, FileSystemLoader, TemplateNotFound, select_autoescape
+from ..models.user import User, Access
 from ..helpers.get_package_path import get_package_path
+from ..helpers.noneless import noneless
 
 FRONTEND_DIRECTORY = get_package_path(__name__, __file__).parent / 'frontend'
 
@@ -11,7 +13,21 @@ env = Environment(
     ),
 )
 
-def render(name: str, context: 'dict | None' = None, response: 'Response | None' = None) -> 'str | Response':
+def nav_menu_template(href, svg, text) -> dict: return {'href': href, 'svg': svg, 'text': text}
+
+async def process_context(context: dict) -> dict:
+    if 'user' in context and isinstance(user := context['user'], User): context['nav_menus'] = noneless((
+        {
+            **nav_menu_template(None, 'sack', 'Zis'),
+            'submenus': noneless((
+                nav_menu_template(None, 'in', 'Payments'),
+            ))
+        } if (await user.has_access(Access.ZIS_PAYMENT_READ)) else None,
+    ))
+    return context
+
+async def render(name: str, context: 'dict | None' = None, response: 'Response | None' = None) -> 'str | Response':
+    context = await process_context(context or {})
     possible_names = (*(possible_name for possible_name in (
         name,
         name[:-1] if len(name) > 1 and name[-1] in ('/', '\\') else None,
@@ -21,7 +37,7 @@ def render(name: str, context: 'dict | None' = None, response: 'Response | None'
     content = None
     error = None
     for possible_name in possible_names:
-        try: content = env.get_template(possible_name).render(context or {})
+        try: content = env.get_template(possible_name).render(context)
         except TemplateNotFound as e:
             if error is None: error = e
             continue
