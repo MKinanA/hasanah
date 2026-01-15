1fdd4922002882271fbf848e14a4754698631187
MKinanA <mkinanahsan@gmail.com>
2026-01-15 15:38:00 +0700

    Minor fix

--- frontend/static/js/payments/index.js
+++ frontend/static/js/payments/index.js
@@ -2,7 +2,7 @@ import {} from '../utils/auth.js';
 import { leadingZeros } from '../utils/formatting.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('body > div > main > table > tbody > tr > td:is(:nth-child(2), :nth-child(4)) > p').forEach(cell => {
+    document.querySelectorAll('body > div > main > div#table > table > tbody > tr > td:is(:nth-child(2), :nth-child(4)) > p').forEach(cell => {
         const date = new Date(parseInt(cell.textContent) * 1000);
         cell.textContent = `${leadingZeros(date.getDate(), 2)}/${leadingZeros(date.getMonth() + 1, 2)}/${leadingZeros(date.getFullYear(), 2)} ${leadingZeros(date.getHours(), 2)}:${leadingZeros(date.getMinutes(), 2)}:${leadingZeros(date.getSeconds(), 2)}`
     });
