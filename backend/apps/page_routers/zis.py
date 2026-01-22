4c5167b946f1a81c10bc2a4570272ac317002c6e
MKinanA <mkinanahsan@gmail.com>
2026-01-22 11:34:24 +0700

    Menambahkan route untuk halaman edit payment

--- backend/apps/page_routers/zis.py
+++ backend/apps/page_routers/zis.py
@@ -17,7 +17,7 @@ async def payments_index(request: Request):
         await user.require_access(Access.ZIS_PAYMENT_READ)
     except (NoAuthToken, UserSessionNotFound): return RedirectResponse(url='/login', status_code=302)
     except Access.AccessDenied: return RedirectResponse(url='/home', status_code=302)
-    return await render('pages/zis/payments', {'user': user, 'payments': [(await (await payment.latest).to_dict) for payment in await Payment.query()]})
+    return await render('pages/zis/payments', {'user': user, 'payments': [(await (await payment.latest).to_dict) for payment in await Payment.query()]}, expose='payments')
 
 @router.get('/payments/{uuid}')
 async def payment(request: Request, response: Response, uuid: str):
@@ -30,4 +30,17 @@ async def payment(request: Request, response: Response, uuid: str):
     if payment is None:
         response.status_code = 404
         return await render('pages/error', {'code': '404', 'error': 'Tidak Ditemukan'})
-    return await render('pages/zis/payments/show', {'user': user, 'payment': await (await payment.latest).to_dict})
+    return await render('pages/zis/payments/show', {'user': user, 'payment': await (await payment.latest).to_dict}, expose='payment')
+
+@router.get('/payments/{uuid}/edit')
+async def payment_edit(request: Request, response: Response, uuid: str):
+    try:
+        user = await auth(request)
+        await user.require_access(Access.ZIS_PAYMENT_READ, Access.ZIS_PAYMENT_WRITE)
+    except (NoAuthToken, UserSessionNotFound): return RedirectResponse(url='/login', status_code=302)
+    except Access.AccessDenied: return RedirectResponse(url=f'/payments/{uuid}' if user.has_access(Access.ZIS_PAYMENT_READ) else '/home', status_code=302)
+    payment = await Payment.get(uuid=uuid)
+    if payment is None:
+        response.status_code = 404
+        return await render('pages/error', {'code': '404', 'error': 'Tidak Ditemukan'})
+    return await render('pages/zis/payments/edit', {'user': user, 'payment': await (await payment.latest).to_dict}, expose='payment')
