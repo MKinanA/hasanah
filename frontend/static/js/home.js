622105587425537d989ac4cafd8e9dbab957df75
MKinanA <mkinanahsan@gmail.com>
2026-01-26 08:08:16 +0700

    Perbaikan di JS home (untuk menentukan pagi, siang, sore, malam)

--- frontend/static/js/home.js
+++ frontend/static/js/home.js
@@ -1,3 +1,3 @@
 const currHour = new Date().getHours();
 
-document.addEventListener('DOMContentLoaded', () => document.querySelector('body > div > main > h1').textContent = document.querySelector('body > div > main > h1').textContent.replace('pagi', currHour >= 11 ? 'siang' : currHour >= 15 ? 'sore' : currHour >= 19 ? 'malam' : 'pagi'));
+document.addEventListener('DOMContentLoaded', () => document.querySelector('body > div > main > h1').textContent = document.querySelector('body > div > main > h1').textContent.replace('pagi', currHour >= 19 ? 'malam' : currHour >= 15 ? 'sore' : currHour >= 11 ? 'siang' : 'pagi'));
