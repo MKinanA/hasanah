de36d1cca7bb55950683d3abee51a141c508dee4
MKinanA <mkinanahsan@gmail.com>
2026-01-28 09:42:20 +0700

    Membuat sub-router baru (`payments`) untuk `zis` di page_routers

--- backend/apps/page_routers/zis.py
+++ backend/apps/page_routers/zis.py
@@ -10,11 +10,12 @@ from ..dependencies import auth, NoAuthToken, UserSessionNotFound
 from ..render import render
 
 router = APIRouter()
+payments = APIRouter()
 
 @router.get('')
 async def root(): return RedirectResponse(url=f'/{__name__.split(".")[-1]}/payments', status_code=302)
 
-@router.get('/payments')
+@payments.get('')
 async def payments_index(request: Request):
     try:
         user = await auth(request)
@@ -23,7 +24,7 @@ async def payments_index(request: Request):
     except Access.AccessDenied: return RedirectResponse(url='/home', status_code=302)
     return await render('pages/zis/payments', {'user': user, 'payments': [(await (await payment.latest).to_dict) for payment in await Payment.query()]}, expose='payments')
 
-@router.get('/payments/export-xlsx')
+@payments.get('/export-xlsx')
 async def payments_xlsx(request: Request):
     try:
         user = await auth(request)
@@ -106,7 +107,7 @@ async def payments_xlsx(request: Request):
         headers={'Content-Disposition': f'attachment; filename=Export_Pembayaran_ZIS_{int(time())}.xlsx'}
     )
 
-@router.get('/payments/new')
+@payments.get('/new')
 async def payment_new(request: Request, response: Response):
     try:
         user = await auth(request)
@@ -115,7 +116,7 @@ async def payment_new(request: Request, response: Response):
     except Access.AccessDenied: return RedirectResponse(url=f'/payments' if user.has_access(Access.ZIS_PAYMENT_READ) else '/home', status_code=302)
     return await render('pages/zis/payments/new', {'user': user, 'categories': (*({'value': category, 'name': category.capitalize()} for id, category in (await PaymentCategory.get_all()).items()),), 'units': (*({'value': unit, 'name': unit.capitalize()} for id, unit in (await PaymentUnit.get_all()).items()),)})
 
-@router.get('/payments/{uuid}')
+@payments.get('/{uuid}')
 async def payment(request: Request, response: Response, uuid: str):
     try:
         user = await auth(request)
@@ -126,7 +127,7 @@ async def payment(request: Request, response: Response, uuid: str):
     if payment is None: return await render('pages/error', {'code': '404', 'error': 'Pembayaran Tidak Ditemukan', 'user': user}, status_code=404)
     return await render('pages/zis/payments/show', {'user': user, 'payment': await (await payment.latest).to_dict}, expose='payment')
 
-@router.get('/payments/{uuid}/edit')
+@payments.get('/{uuid}/edit')
 async def payment_edit(request: Request, response: Response, uuid: str):
     try:
         user = await auth(request)
@@ -136,3 +137,5 @@ async def payment_edit(request: Request, response: Response, uuid: str):
     payment = await Payment.get(uuid=uuid)
     if payment is None: return await render('pages/error', {'code': '404', 'error': 'Pembayaran Tidak Ditemukan', 'user': user}, status_code=404)
     return await render('pages/zis/payments/edit', {'user': user, 'payment': await (await payment.latest).to_dict, 'categories': (*({'value': category, 'name': category.capitalize()} for id, category in (await PaymentCategory.get_all()).items()),), 'units': (*({'value': unit, 'name': unit.capitalize()} for id, unit in (await PaymentUnit.get_all()).items()),)}, expose='payment')
+
+router.include_router(payments, prefix='/payments')
