e56fcf921e64e477ed048bf50b2ff4727ed42535
MKinanA <mkinanahsan@gmail.com>
2026-01-30 14:51:47 +0700

    Minor tweak

--- frontend/static/js/error.js
+++ frontend/static/js/error.js
@@ -3,9 +3,9 @@ import { delay } from './utils/async.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     const timer = document.querySelector('body > div > main > p > span');
-    for (let i = parseInt(timer.textContent) || 10; i >= 0; i--) {
-        timer.textContent = i;
+    for (let i = (parseInt(timer.textContent) || 10) - 1; i >= 0; i--) {
         await delay(1000);
+        timer.textContent = i;
     };
     setTimeout(() => location.replace('/'), 1000);
     history.back();
