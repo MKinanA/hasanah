ccd0cdddaa9da9f2db15457a236452e2bfff2d85
MKinanA <mkinanahsan@gmail.com>
2026-01-30 17:57:09 +0700

    Tombol akses hanya akan keluar jika halaman users show dibuka oleh admin atau user itu sendiri

--- backend/apps/page_routers/users.py
+++ backend/apps/page_routers/users.py
@@ -19,4 +19,4 @@ async def user(request: Request, username: str):
     except (NoAuthToken, UserSessionNotFound): pass
     target_user = await User.get(username=username)
     if target_user is None: return await render('pages/error', {'code': '404', 'error': 'User Tidak Ditemukan', **({'user': user} if user is not None else {'use_header': False})}, status_code=404)
-    return await render('pages/users/show', {**({'user': user} if user is not None else {'use_header': False}), 'target_user': {'username': target_user.username, 'name': target_user.name}}, expose='target_user')
+    return await render('pages/users/show', {**({'user': user, 'show_admin_options': await user.has_access(Access.ADMIN)} if user is not None else {'use_header': False}), 'target_user': {'username': target_user.username, 'name': target_user.name}}, expose='target_user')
