863dd2e842ad8238cc1fe353cc81e3f3d3dbdbb9
MKinanA <mkinanahsan@gmail.com>
2026-01-30 16:19:09 +0700

    Menambahkan dialog konfirmasi sebelum logout

--- frontend/static/js/base.js
+++ frontend/static/js/base.js
@@ -1,3 +1,5 @@
 import { storeToken } from './utils/auth.js';
 
 storeToken(localStorage.getItem('token'));
+
+document.addEventListener('DOMContentLoaded', () => document.querySelectorAll('a:is([href="/logout"], [href="/logout/"])').forEach(element => element.addEventListener('click', e => confirm('Logout dari user anda saat ini? Anda akan harus login kembali.') ? true : e.preventDefault())));
