f1fe3e5e7d12f5aa3226916336e2858c5164bff4
MKinanA <mkinanahsan@gmail.com>
2026-03-09 00:23:42 +0700

    Memperbarui helper log agar mencetak angka pid (process id (kayaknya))

--- backend/helpers/log.py
+++ backend/helpers/log.py
@@ -1,3 +1,4 @@
+from os import getpid
 from datetime import datetime
 from colorama import Fore, Back, Style
 # This `log.py` file MUST NOT import any other helpers or files as they all import this file
@@ -14,5 +15,6 @@ default_level = 'INFO'
 prefix = ''
 suffix = '.'
 nothing = ''
+pid = str(getpid())
 
-def log(name: str, message: str, level: str = default_level, level_color: 'str | None' = None) -> str: return f'{Style.RESET_ALL + Style.DIM + datetime.now().strftime(strftime) + Style.RESET_ALL} {Fore.BLUE + Style.BRIGHT + name + Style.RESET_ALL}: [{Style.BRIGHT + (level_colors[level_color.upper() if level_color != None and level_color.upper() in level_colors else level.upper() if level.upper() in level_colors else default_level]) + level + Style.RESET_ALL}] {((message[0].upper() + message[1:]) if len(message) > 0 else message) + (suffix if not message.endswith(suffix) else nothing)}'
+def log(name: str, message: str, level: str = default_level, level_color: 'str | None' = None) -> str: return f'{Style.RESET_ALL + Style.DIM + datetime.now().strftime(strftime) + Style.RESET_ALL} {Fore.CYAN + Style.BRIGHT + pid + Style.RESET_ALL} {Fore.BLUE + Style.BRIGHT + name + Style.RESET_ALL}: [{Style.BRIGHT + (level_colors[level_color.upper() if level_color != None and level_color.upper() in level_colors else level.upper() if level.upper() in level_colors else default_level]) + level + Style.RESET_ALL}] {((message[0].upper() + message[1:]) if len(message) > 0 else message) + (suffix if not message.endswith(suffix) else nothing)}'
