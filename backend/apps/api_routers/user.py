54ecc1c8b8f5b0fae191aeba8ffface7b739095a
MKinanA <mkinanahsan@gmail.com>
2026-03-09 11:06:52 +0700

    Menambahkan endpoint untuk grant dan revoke access, serta memperbaiki endpoint user (`/`)

--- backend/apps/api_routers/user.py
+++ backend/apps/api_routers/user.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request, Response, Depends
-from ...models.user import User
+from ...models.user import User, Access
 from ...helpers.api_response import api_response as mkresp
 from ..dependencies import auth, json_body
 
@@ -7,8 +7,27 @@ router = APIRouter()
 
 @router.post('')
 async def user(request: Request, response: Response, user: User = Depends(auth), body: dict = Depends(json_body)) -> dict:
-    requested_user = await User.get(**body)
+    try: requested_user = await User.get(username=body['username'])
+    except KeyError:
+        response.status_code = 400
+        return mkresp('error', 'Bad Request', 'Please pass a username to look for.')
     if requested_user is None:
         response.status_code = 404
         return mkresp('error', 'Not Found', 'The requested user is not found based on the provided queries.')
-    return mkresp('success', 'User Found', 'User info fetched successfully.', username=user.username, name=user.name)
+    return mkresp('success', 'User Found', 'User info fetched successfully.', username=requested_user.username, name=requested_user.name, **({'accesses': await requested_user.accesses} if user.has_access(Access.ADMIN) else {}))
+
+@router.post('/grant-access')
+async def grant_access(request: Request, response: Response, user_resp: dict = Depends(user), body: dict = Depends(json_body)):
+    if user_resp['type'] != 'success': return user_resp
+    user = await User.get(username=user_resp['username'])
+    if user is None: raise RuntimeError('Failed to fetch user from username.')
+    await user.grant_access(access := body['access'])
+    return mkresp('success', 'Access Granted', 'Access has been successfully granted to user.', username=user.username, name=user.name, access=access)
+
+@router.post('/revoke-access')
+async def revoke_access(request: Request, response: Response, user_resp: dict = Depends(user), body: dict = Depends(json_body)):
+    if user_resp['type'] != 'success': return user_resp
+    user = await User.get(username=user_resp['username'])
+    if user is None: raise RuntimeError('Failed to fetch user from username.')
+    await user.revoke_access(access := body['access'])
+    return mkresp('success', 'Access Revoked', 'Access has been successfully revoked from user.', username=user.username, name=user.name, access=access)
