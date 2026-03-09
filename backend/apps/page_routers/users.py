4cfbd0df051efa11dfa4008e0063ad77ab775262
MKinanA <mkinanahsan@gmail.com>
2026-03-09 12:29:07 +0700

    Menambahkan endpoint untuk akses user

--- backend/apps/page_routers/users.py
+++ backend/apps/page_routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Response, Depends
 from fastapi.responses import RedirectResponse
 from ..render import render
 from ..dependencies import auth, NoAuthToken, UserSessionNotFound
@@ -20,3 +20,12 @@ async def user(request: Request, username: str):
     target_user = await User.get(username=username)
     if target_user is None: return await render('pages/error', {'code': '404', 'error': 'User Tidak Ditemukan', **({'user': user} if user is not None else {'use_header': False})}, status_code=404)
     return await render('pages/users/show', {**({'user': user, 'show_admin_options': await user.has_access(Access.ADMIN)} if user is not None else {'use_header': False}), 'target_user': {'username': target_user.username, 'name': target_user.name}}, expose='target_user')
+
+@router.get('/{username}/accesses')
+async def accesses(request: Request, username: str):
+    try: user = await auth(request)
+    except (NoAuthToken, UserSessionNotFound): return RedirectResponse(url='/login', status_code=302)
+    if not await user.has_access(Access.ADMIN): return await render('pages/error', {'code': '403', 'error': 'Akses Ditolak', 'user': user}, status_code=403)
+    target_user = await User.get(username=username)
+    if target_user is None: return await render('pages/error', {'code': '404', 'error': 'User Tidak Ditemukan', 'user': user}, status_code=404)
+    return await render('pages/users/accesses', {'user': user, 'target_user': {'username': target_user.username, 'name': target_user.name}}, expose='target_user')
