9cdb3c049f4c18460c3386f5ee88264e94fe4f0f
MKinanA <mkinanahsan@gmail.com>
2026-03-09 14:03:08 +0700

    Akses admin dimasukkan lagi ke list akses user

--- backend/apps/page_routers/users.py
+++ backend/apps/page_routers/users.py
@@ -28,4 +28,4 @@ async def accesses(request: Request, username: str):
     if not await user.has_access(Access.ADMIN): return await render('pages/error', {'code': '403', 'error': 'Akses Ditolak', 'user': user}, status_code=403)
     target_user = await User.get(username=username)
     if target_user is None: return await render('pages/error', {'code': '404', 'error': 'User Tidak Ditemukan', 'user': user}, status_code=404)
-    return await render('pages/users/accesses', {'user': user, 'target_user': {'username': target_user.username, 'name': target_user.name}, 'accesses': {access: await target_user.has_access(access, allow_admin=False) for access in (await Access.get_all()).values() if access != Access.ADMIN}}, expose='target_user')
+    return await render('pages/users/accesses', {'user': user, 'target_user': {'username': target_user.username, 'name': target_user.name}, 'accesses': {access: await target_user.has_access(access, allow_admin=False) for access in (await Access.get_all()).values()}, 'admin': Access.ADMIN}, expose='target_user')
