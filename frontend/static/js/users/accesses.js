fcb97e5978d87bdd777e045d3453ef9b91eb1aad
MKinanA <mkinanahsan@gmail.com>
2026-03-09 14:53:36 +0700

    Minor fix

--- frontend/static/js/users/accesses.js
+++ frontend/static/js/users/accesses.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
         })});
         const body = await resp.json();
         if (!(Math.floor(resp.status / 100) === 2) || !(body.type === 'success')) {
-            e.preventDefault();
             alert(`Error: ${body.message ?? body.detail ?? '?'}`);
+            toggle.checked = !toggle.checked;
         };
         document.querySelector(`body > div > main > div > label:has(> input[type="checkbox"][name="${toggle.getAttribute('name')}"])`).classList.remove('loading');
     }))(toggle)));
