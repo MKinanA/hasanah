56aad4da643e4acbe5a55e2d1a8bce90b71dbd59
MKinanA <mkinanahsan@gmail.com>
2026-03-09 14:51:17 +0700

    Menambahkan fungsionalitas untuk toggle akses user

---
+++ frontend/static/js/users/accesses.js
@@ -0,0 +1,19 @@
+import { getData } from '../utils/data.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const data = getData();
+
+    document.querySelectorAll('body > div > main > div > label > input[type="checkbox"]').forEach(toggle => toggle.addEventListener('click', (toggle => (async e => {
+        document.querySelector(`body > div > main > div > label:has(> input[type="checkbox"][name="${toggle.getAttribute('name')}"])`).classList.add('loading');
+        const resp = await fetch(`/api/user/${toggle.checked ? 'grant-access' : 'revoke-access'}`, {method: 'POST', body: JSON.stringify({
+            username: data.target_user.username,
+            access: toggle.getAttribute('name'),
+        })});
+        const body = await resp.json();
+        if (!(Math.floor(resp.status / 100) === 2) || !(body.type === 'success')) {
+            e.preventDefault();
+            alert(`Error: ${body.message ?? body.detail ?? '?'}`);
+        };
+        document.querySelector(`body > div > main > div > label:has(> input[type="checkbox"][name="${toggle.getAttribute('name')}"])`).classList.remove('loading');
+    }))(toggle)));
+});
