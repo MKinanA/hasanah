6c14da5d01544ea1e70019554659d25b1d0960ec
MKinanA <mkinanahsan@gmail.com>
2026-03-11 13:28:45 +0700

    Menambahkan endpoint API untuk update nama user

--- backend/apps/api_routers/user.py
+++ backend/apps/api_routers/user.py
@@ -13,8 +13,19 @@ async def user(request: Request, response: Response, user: User = Depends(auth),
         return mkresp('error', 'Bad Request', 'Please pass a username to look for.')
     if requested_user is None:
         response.status_code = 404
-        return mkresp('error', 'Not Found', 'The requested user is not found based on the provided queries.')
+        return mkresp('error', 'Not Found', 'The requested user is not found based on the provided username.')
     return mkresp('success', 'User Found', 'User info fetched successfully.', username=requested_user.username, name=requested_user.name, **({'accesses': await requested_user.accesses} if await user.has_access(Access.ADMIN) else {}))
+
+@router.post('/update-name')
+async def update_name(request: Request, response: Response, user: User = Depends(auth), user_resp: dict = Depends(user), body: dict = Depends(json_body)):
+    if user_resp['type'] != 'success': return user_resp
+    target_user = await User.get(username=user_resp['username'])
+    if target_user is None: raise RuntimeError('Failed to fetch user from username.')
+    if not (target_user.username == user.username or user.has_access(Access.ADMIN)):
+        response.status_code = 403
+        return mkresp('error', 'Access Denied', 'You have no authority to update this user\'s name.')
+    target_user.username = body.get('new_name') or ''
+    return mkresp('success', 'Name Updated', 'User\'s name has been updated successfully.', username=target_user.username, name=target_user.name)
 
 @router.post('/grant-access')
 async def grant_access(request: Request, response: Response, user: User = Depends(auth), user_resp: dict = Depends(user), body: dict = Depends(json_body)):
